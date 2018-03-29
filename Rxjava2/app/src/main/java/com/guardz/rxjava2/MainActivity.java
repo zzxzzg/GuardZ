cd2fbc748b0d6e99e9d845d680e6fc25146cbd51
王彦骁 <xiaobanma2292@gmail.com>
2018-03-29 23:13:24 +0800

    nothing update

--- Rxjava2/app/src/main/java/com/guardz/rxjava2/MainActivity.java
+++ Rxjava2/app/src/main/java/com/guardz/rxjava2/MainActivity.java
@@ -3,11 +3,14 @@ package com.guardz.rxjava2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.guardz.rxjava2lib.TestClass;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TestClass testClass = new TestClass();
     }
 }
--- Rxjava2/rxjava2lib/src/main/java/com/guardz/rxjava2lib/TestClass.java
+++ Rxjava2/rxjava2lib/src/main/java/com/guardz/rxjava2lib/TestClass.java
@@ -1,11 +1,22 @@
 package com.guardz.rxjava2lib;
 
+import android.util.Log;
+
+import org.reactivestreams.Subscription;
+
+import io.reactivex.Flowable;
+import io.reactivex.FlowableSubscriber;
 import io.reactivex.Observable;
 import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.ObservableOperator;
 import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
+import io.reactivex.functions.LongConsumer;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * Created by yxwang on 17/3/30.
@@ -13,14 +24,15 @@ import io.reactivex.functions.Consumer;
 
 public class TestClass {
     public TestClass(){
-        Observable.just("1").subscribe(new Observer<String>() {
+        Observable.just("1").subscribeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.newThread()).subscribe(new Observer<String>() {
             @Override
             public void onSubscribe(Disposable d) {
+                Log.d("sss","onSubscribe current thread is "+Thread.currentThread().getName());
             }
 
             @Override
             public void onNext(String s) {
-
+                Log.d("sss","onNext current thread is "+Thread.currentThread().getName());
             }
 
             @Override
@@ -33,6 +45,37 @@ public class TestClass {
 
             }
         });
+        Observable.just("1").lift(new ObservableOperator<Integer, String>() {
+            @Override
+            public Observer<? super String> apply(@NonNull final Observer<? super Integer> observer) throws Exception {
+                return new Observer<String>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        observer.onSubscribe(d);
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        observer.onNext(Integer.parseInt(s));
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        observer.onError(e);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        observer.onComplete();
+                    }
+                };
+            }
+        }).subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(@NonNull Integer integer) throws Exception {
+
+            }
+        });
 
         Observable.just("1").subscribe(new Consumer<String>() {
             @Override
@@ -42,6 +85,28 @@ public class TestClass {
         }, new Consumer<Throwable>() {
             @Override
             public void accept(@NonNull Throwable throwable) throws Exception {
+
+            }
+        });
+
+        Flowable.just("1").subscribe(new FlowableSubscriber<String>() {
+            @Override
+            public void onSubscribe(@NonNull Subscription s) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onComplete() {
 
             }
         });
